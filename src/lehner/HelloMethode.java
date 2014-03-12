db2fb31efb3e9d48dc7605ecd661fa7c1545a95d
Cindy Lehner <clehner@student.tgm.ac.at>
2014-03-12 14:53:39 +0100

    Zweite Methode

--- src/lehner/HelloMethode.java
+++ src/lehner/HelloMethode.java
@@ -5,4 +5,9 @@ public class HelloMethode {
 	public static void hello(String name) {
 		System.out.println("Hallo " + name);
 	}
+	
+	public static void merhaba(String name) {
+		System.out.println("Merhaba " + name);
+	}
+	
 }
--- src/lehner/Main.java
+++ src/lehner/Main.java
@@ -5,6 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		HelloMethode.hello("Cindy");
+		HelloMethode.merhaba("Sarah");
 	}
+	
+	
 
 }
